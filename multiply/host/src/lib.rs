82ddf36a4c2f7d02d209b062042ef27fd6099557
Jan Strelec <jan.strelec853@gmail.com>
2025-05-17 21:11:16 +0200

    Init project

---
+++ multiply/host/src/lib.rs
@@ -0,0 +1,56 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+use multiply_methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
+
+// This is a Hello World demo for the RISC Zero zkVM.
+// By running the demo, Alice can produce a receipt that proves that she knows
+// some numbers a and b, such that a*b == 391.
+// The factors a and b are kept secret.
+
+// Compute the product a*b inside the zkVM
+pub fn multiply(a: u64, b: u64) -> (Receipt, u64) {
+    let env = ExecutorEnv::builder()
+        // Send a & b to the guest
+        .write(&a)
+        .unwrap()
+        .write(&b)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    let receipt = prover.prove(env, MULTIPLY_ELF).unwrap().receipt;
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: u64 = receipt.journal.decode().expect(
+        "Journal output should deserialize into the same types (& order) that it was written",
+    );
+    let _verification = match receipt.verify(MULTIPLY_ID){
+        Ok(()) => println!("Proof is Valid"),
+        Err(_) => println!("Something went wrong !!"),
+    };
+    // Report the product
+    println!("I know the factors of {}, and I can prove it!", c);
+
+    (receipt, c)
+}
+
+
+
