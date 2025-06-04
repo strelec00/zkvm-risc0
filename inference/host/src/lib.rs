d5bf40db2539186531e2529cbf29b05a0630fec3
Jan Strelec <jan.strelec853@gmail.com>
2025-06-04 19:51:07 +0200

    Try with linux, uses prover::groth16

--- inference/host/src/lib.rs
+++ inference/host/src/lib.rs
@@ -1,9 +1,6 @@
 use mnist_methods::MNIST_ELF;
 use risc0_zkvm::{ExecutorEnv, ProverOpts, Receipt};
 use risc0_zkvm::serde::from_slice;
-use risc0_groth16::to_json;
-use std::fs::File;
-use std::io::{Cursor, Write};
 
 pub fn export_receipt(input: &[i32; 784]) {
     // Izgradnja izvršnog okruženja
@@ -12,16 +9,14 @@ pub fn export_receipt(input: &[i32; 784]) {
         .build().unwrap();
 
     // Konfiguriranje opcija prover-a za Groth16
-    let prover_opts = ProverOpts::default();
+    let prover_opts = ProverOpts::groth16();
+
     // Provođenje dokazivanja
     let prover = risc0_zkvm::default_prover();
     let session = prover.prove_with_opts(env, MNIST_ELF, &prover_opts).unwrap();
     let receipt: Receipt = session.receipt;
 
-
     // Deserijalizacija predviđene vrijednosti iz dnevnika
-    let predicted: i32 = from_slice(&*receipt.journal.bytes).unwrap();
+    let predicted: i32 = from_slice(&receipt.journal.bytes).unwrap();
     println!("Predviđeno: {}", predicted);
-
-
 }
