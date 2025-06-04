583c1588ca7ee6378ab567153b8440c3645e403e
Jan Strelec <jan.strelec853@gmail.com>
2025-06-04 14:55:18 +0200

    Maybe works on linux, must try when I get on Linux

--- inference/host/src/lib.rs
+++ inference/host/src/lib.rs
@@ -12,32 +12,16 @@ pub fn export_receipt(input: &[i32; 784]) {
         .build().unwrap();
 
     // Konfiguriranje opcija prover-a za Groth16
-    let prover_opts = ProverOpts::groth16();
-
+    let prover_opts = ProverOpts::default();
     // Provođenje dokazivanja
     let prover = risc0_zkvm::default_prover();
     let session = prover.prove_with_opts(env, MNIST_ELF, &prover_opts).unwrap();
     let receipt: Receipt = session.receipt;
 
+
     // Deserijalizacija predviđene vrijednosti iz dnevnika
     let predicted: i32 = from_slice(&*receipt.journal.bytes).unwrap();
     println!("Predviđeno: {}", predicted);
 
-    // Serijalizacija dokaza u bajtove
-    let encoded_receipt = bincode::serialize(&receipt).unwrap();
 
-    // Pisanje binarnog dokaza u datoteku
-    let mut bin_file = File::create("proof.bin").unwrap();
-    bin_file.write_all(&encoded_receipt).unwrap();
-
-    // Kreiranje kursora za čitanje serijaliziranih bajtova
-    let reader = Cursor::new(encoded_receipt);
-
-    // Otvaranje datoteke za JSON izlaz
-    let mut json_file = File::create("proof.json").unwrap();
-
-    // Pretvaranje binarnog dokaza u JSON i pisanje u datoteku
-    to_json(reader, &mut json_file).unwrap();
-
-    // Sada proof.json sadrži JSON verziju dokaza
 }
